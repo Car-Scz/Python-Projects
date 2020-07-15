ca92b17ed059a6da771f1f777373df2d58eaa0d3
Carol Schultz <carschul24@gmail.com>
2020-07-14 17:19:55 -0700

    Class Inheritance Example
    Created two child classes that inherited attributes from another class.

---
+++ createClass.py
@@ -0,0 +1,18 @@
+#  This is a cat class
+class Book:
+    #  Cat class attributes defined
+    name = ' '
+    author = ' '
+    typeFace =  ' '
+    medium = ' '
+    language = ' '
+                    
+class Category(Book):
+    genre = ' '
+    subgenre = ' '
+
+class Publishing(Book):
+    company = ' '
+    publishDate = 0
+    ISBN = ' '
+    
